4bffa88757d3a48491b8673aa8c0f8395981a92b
2637309949@qq.com <2637309949@qq.com>
2022-01-06 14:19:08 +0800

    feat: 修改文档

--- client/cli/new/template/readme.go
+++ client/cli/new/template/readme.go
@@ -1,18 +1,7 @@
 package template
 
 var (
-	Readme = `# {{title .Alias}} Service
-
-This is the {{title .Alias}} service
-
-Generated with
-
-` + "```" +
-		`
-micro new {{.Alias}}
-` + "```" + `
-
-## Usage
+	Readme = `## Usage
 
 Generate the proto code
 
