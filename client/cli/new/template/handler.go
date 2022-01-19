7ce72a61c3f23b7263ee9ed54cc23c60c5ab55af
2637309949@qq.com <2637309949@qq.com>
2022-01-19 11:20:50 +0800

    feat: 修改new模板

--- client/cli/new/template/handler.go
+++ client/cli/new/template/handler.go
@@ -17,14 +17,48 @@ import (
 	{{dehyphen .Alias}} "proto/{{.Dir}}"
 )
 
-// Call is a single request handler called via client.Call or the generated client code
-func (h *Handler) Call(ctx context.Context, req *{{dehyphen .Alias}}.Request, rsp *{{dehyphen .Alias}}.Response) error {
+// DeleteInfo defined TODO
+func (h *Handler) DeleteInfo(ctx context.Context, req *{{dehyphen .Alias}}.DeleteInfoRequest, rsp *{{dehyphen .Alias}}.DeleteInfoResponse) error {
 	acc, ok := auth.FromContext(ctx)
 	if ok {
-		logger.Infof(ctx, "%v Do Call", acc.Name)
+		logger.Infof(ctx, "%v Do DeleteInfo", acc.Name)
 	}
-	logger.Info(ctx, "Received {{title .Alias}}.Call request")
-	rsp.Msg = "Hello " + req.Name
+	return nil
+}
+
+// UpdateInfo defined TODO
+func (h *Handler) UpdateInfo(ctx context.Context, req *{{dehyphen .Alias}}.UpdateInfoRequest, rsp *{{dehyphen .Alias}}.UpdateInfoResponse) error {
+	acc, ok := auth.FromContext(ctx)
+	if ok {
+		logger.Infof(ctx, "%v Do UpdateInfo", acc.Name)
+	}
+	return nil
+}
+
+// InsertInfo defined TODO
+func (h *Handler) InsertInfo(ctx context.Context, req *{{dehyphen .Alias}}.InsertInfoRequest, rsp *{{dehyphen .Alias}}.InsertInfoResponse) error {
+	acc, ok := auth.FromContext(ctx)
+	if ok {
+		logger.Infof(ctx, "%v Do InsertInfo", acc.Name)
+	}
+	return nil
+}
+
+// QueryInfoDetail defined TODO
+func (h *Handler) QueryInfoDetail(ctx context.Context, req *{{dehyphen .Alias}}.QueryInfoDetailRequest, rsp *{{dehyphen .Alias}}.QueryInfoDetailResponse) error {
+	acc, ok := auth.FromContext(ctx)
+	if ok {
+		logger.Infof(ctx, "%v Do QueryInfoDetail", acc.Name)
+	}
+	return nil
+}
+
+// QueryInfo defined TODO
+func (h *Handler) QueryInfo(ctx context.Context, req *{{dehyphen .Alias}}.QueryInfoRequest, rsp *{{dehyphen .Alias}}.QueryInfoResponse) error {
+	acc, ok := auth.FromContext(ctx)
+	if ok {
+		logger.Infof(ctx, "%v Do QueryInfo", acc.Name)
+	}
 	return nil
 }
 `
--- client/cli/new/template/proto.go
+++ client/cli/new/template/proto.go
@@ -10,7 +10,11 @@ option go_package = "./proto;{{dehyphen .Alias}}";
 import "proto/{{dehyphen .Alias}}/{{dehyphen .Alias}}.proto";
 
 service {{title .Alias}} {
-	rpc Call(Request) returns (Response) {}
+    rpc InsertInfo (InsertInfoRequest) returns (InsertInfoResponse) {}
+	rpc DeleteInfo (DeleteInfoRequest) returns (DeleteInfoResponse) {}
+    rpc UpdateInfo (UpdateInfoRequest) returns (UpdateInfoResponse) {}
+    rpc QueryInfo (QueryInfoRequest) returns (QueryInfoResponse) {}
+    rpc QueryInfoDetail (QueryInfoDetailRequest) returns (QueryInfoDetailResponse) {}
 }
 `
 	ProtoModelSRV = `syntax = "proto3";
@@ -19,14 +23,60 @@ package {{dehyphen .Alias}};
 
 option go_package = "./proto;{{dehyphen .Alias}}";
 
-message Request {
+message InsertInfoRequest {
 	string name = 1 [json_name = "name"];
-    uint64 operator_id = 2 [json_name = "operator_id"];
-    string operator_name = 3 [json_name = "operator_name"];
 }
 
-message Response {
-	string msg = 1;
+message InsertInfoResponse {
+	uint32 id = 1 [json_name = "id"];
+}
+
+message DeleteInfoRequest {
+	uint32 id = 1 [json_name = "id"];
+}
+
+message DeleteInfoResponse {
+	uint32 id = 1 [json_name = "id"];
+}
+
+message UpdateInfoRequest {
+	uint32 id = 1 [json_name = "id"];
+	string name = 2 [json_name = "name"];
+}
+
+message UpdateInfoResponse {
+	uint32 id = 1 [json_name = "id"];
+}
+
+message QueryInfoRequest {
+	uint32 id = 1 [json_name = "id"];
+	string name = 2 [json_name = "name"];
+
+	int32 page = 3 [json_name = "page"];
+	int32 size = 4 [json_name = "size"];
+	int32 order = 5 [json_name = "order"];
+}
+
+message QueryInfoResponseItem {
+	uint32 id = 1 [json_name = "id"];
+	string name = 2 [json_name = "name"];
+}
+
+message QueryInfoResponse {
+    repeated QueryInfoResponseItem data = 1 [json_name = "data"];
+
+	int32 page = 2 [json_name = "page"];
+	int32 size = 3 [json_name = "size"];
+	int32 total_count = 4 [json_name = "total_count"];
+}
+
+message QueryInfoDetailRequest {
+	uint32 id = 1 [json_name = "id"];
+}
+
+message QueryInfoDetailResponse {
+	uint32 id = 1 [json_name = "id"];
+	string name = 2 [json_name = "name"];
 }
 `
 )
